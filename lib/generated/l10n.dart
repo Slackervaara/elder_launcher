b68d2cdeff2a6cbd8fe80cb95991dd3361f77340
Slackervaara <156539232+Slackervaara@users.noreply.github.com>
2025-08-17 14:32:57 +0200

    Update l10n.dart

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -318,6 +318,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
       Locale.fromSubtags(languageCode: 'pl'),
       Locale.fromSubtags(languageCode: 'pt'),
       Locale.fromSubtags(languageCode: 'ru'),
+      Locale.fromSubtags(languageCode: 'sv'),
     ];
   }
 
